c2c88d6835a29245642d41ed82f5167ffd05619b
OmranInTech <ahmadzai.omran12@gmail.com>
2026-08-12 11:36:06 +0430

    test folder deleted

--- Netlinks_backend_project/src/main.ts
+++ Netlinks_backend_project/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import {
+  I18nValidationExceptionFilter,
+  I18nValidationPipe,
+} from 'nestjs-i18n';
+
 import { AppModule } from './app.module';
-import {I18nValidationExceptionFilter,I18nValidationPipe,} from 'nestjs-i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Validation
   app.useGlobalPipes(
     new I18nValidationPipe(),
   );
@@ -12,6 +21,21 @@ async function bootstrap() {
   app.useGlobalFilters(
     new I18nValidationExceptionFilter(),
   );
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Taxi Backend API')
+    .setDescription(
+      'API documentation for the Taxi Backend application',
+    )
+    .setVersion('1.0')
+    .addTag('Signup')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(3000);
 }
