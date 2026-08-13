629ee671ad5233b6c23dbb4f254d4500aa687b2b
OmranInTech <ahmadzai.omran12@gmail.com>
2026-08-13 10:25:44 +0430

    2fa added

--- Netlinks_backend_project/src/modules/users/user.controller.ts
+++ Netlinks_backend_project/src/modules/users/user.controller.ts
@@ -1,14 +1,8 @@
-import {
-  Body,
-  Controller,
-  Post,
-} from '@nestjs/common';
-
+import {Body,Controller,Post,} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifySignupDto } from './dto/verify-signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { VerifyLoginDto } from './dto/verify-login.dto';
-
 import { UserService } from './user.service';
 
 @Controller()
@@ -17,36 +11,20 @@ export class UserController {
     private readonly userService: UserService,
   ) {}
 
-  // =========================
-  // SIGNUP
-  // =========================
-
   @Post('signup')
   async signup(@Body() dto: CreateUserDto) {
     return this.userService.signup(dto);
   }
-
-  // =========================
-  // VERIFY SIGNUP
-  // =========================
 
   @Post('signup/verify')
   async verifySignup(@Body() dto: VerifySignupDto) {
     return this.userService.verifySignup(dto);
   }
 
-  // =========================
-  // LOGIN
-  // =========================
-
   @Post('login')
   async login(@Body() dto: LoginDto) {
     return this.userService.login(dto);
   }
-
-  // =========================
-  // VERIFY LOGIN OTP
-  // =========================
 
   @Post('login/verify')
   async verifyLogin(@Body() dto: VerifyLoginDto) {
